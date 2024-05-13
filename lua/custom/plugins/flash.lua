bccdb7696a33def47ef5c7654c8ca3a4adeb9785
Vamsi Talupula <tanavamsikrishna@outlook.com>
2024-05-13 12:17:52 +0530

    ''

---
+++ lua/custom/plugins/flash.lua
@@ -0,0 +1,24 @@
+return {
+  'folke/flash.nvim',
+  opts = {
+    label = { uppercase = false },
+  },
+  keys = {
+    {
+      '<leader>fs',
+      function()
+        require('flash').treesitter()
+      end,
+      mode = 'n',
+      desc = 'Select based on syntax',
+    },
+    {
+      '<leader>fj',
+      function()
+        require('flash').jump()
+      end,
+      mode = 'n',
+      desc = 'Jump to location',
+    },
+  },
+}
