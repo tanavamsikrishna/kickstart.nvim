2c32ec159ec9c56ac18805340c6148cf3940aa7c
Vamsi Talupula <tanavamsikrishna@outlook.com>
2024-05-14 21:34:33 +0530

    ''

--- init.lua
+++ init.lua
@@ -654,12 +654,8 @@ require('lazy').setup({
         lua = { 'stylua' },
         fish = { 'fish_indent' },
         toml = { 'taplo' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use a sub-list to tell conform to run *until* a formatter
-        -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
+        python = { 'ruff_format' },
+        javascript = { { 'prettierd', 'prettier' } },
       },
     },
   },
