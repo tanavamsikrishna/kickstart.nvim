f3f60ec919a0c5b9df5648c1f0c76e85d34d8d4f
Vamsi Talupula <tanavamsikrishna@outlook.com>
2025-03-20 22:08:10 +0530

    ''

--- init.lua
+++ init.lua
@@ -906,10 +906,7 @@ require('lazy').setup({
     'projekt0n/github-nvim-theme',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'github_light_default'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
