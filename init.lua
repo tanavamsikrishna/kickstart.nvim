179c343b54a8bf1501620b2529ef9bdfb16c8a14
Vamsi Talupula <tanavamsikrishna@outlook.com>
2025-03-20 22:01:52 +0530

    ''

--- init.lua
+++ init.lua
@@ -436,6 +436,16 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    build = 'cd app && yarn install',
+    init = function()
+      vim.g.mkdp_filetypes = { 'markdown' }
+    end,
+    ft = { 'markdown' },
+  },
+
   -- LSP Plugins
   {
     -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
