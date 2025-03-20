5f8d817092dcd9cdd2edb851ac9976a8aa8153e0
Vamsi Talupula <tanavamsikrishna@outlook.com>
2025-03-20 22:11:00 +0530

    ''

--- init.lua
+++ init.lua
@@ -453,15 +453,15 @@ require('lazy').setup({
     end,
   },
 
-  {
-    'iamcco/markdown-preview.nvim',
-    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-    build = 'cd app && yarn install',
-    init = function()
-      vim.g.mkdp_filetypes = { 'markdown' }
-    end,
-    ft = { 'markdown' },
-  },
+  -- {
+  --   'iamcco/markdown-preview.nvim',
+  --   cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+  --   build = 'cd app && yarn install',
+  --   init = function()
+  --     vim.g.mkdp_filetypes = { 'markdown' }
+  --   end,
+  --   ft = { 'markdown' },
+  -- },
 
   -- LSP Plugins
   {
