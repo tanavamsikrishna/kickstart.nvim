daec27a35a699d78f466c480da9f40c07e37be92
Vamsi Talupula <tanavamsikrishna@outlook.com>
2025-05-28 21:53:38 +0530

    ''

--- lua/custom/plugins/neovide.lua
+++ lua/custom/plugins/neovide.lua
@@ -4,6 +4,11 @@ if vim.g.neovide then
   vim.g.neovide_cursor_animation_length = 0
   -- vim.opt.linespace = 7
   vim.g.have_nerd_font = true
+  vim.g.neovide_text_gamma = 1.6
+  vim.g.neovide_text_contrast = 0.5
+  -- vim.g.neovide_floating_blur_amount_x = 0
+  -- vim.g.neovide_floating_blur_amount_y = 0.0
+  vim.g.neovide_floating_shadow = true
 
   -- Floating window config
   -- vim.g.neovide_floating_shadow = true
