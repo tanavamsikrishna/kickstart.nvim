7453484cc6f4382cd78267199b367f6218a69ada
Vamsi Talupula <tanavamsikrishna@outlook.com>
2025-06-05 14:49:58 +0530

    ''

--- lua/custom/config/post_lazy.lua
+++ lua/custom/config/post_lazy.lua
@@ -1,5 +1,6 @@
 -- Fix UI issues
 vim.api.nvim_set_hl(0, 'LspSignatureActiveParameter', { bold = true })
 vim.api.nvim_set_hl(0, 'DiagnosticFloatingHint', { link = 'DiagnosticFloatingInfo' })
+vim.api.nvim_set_hl(0, 'MiniStatuslineFilename', { fg = 'black', bg = 'white', italic = true })
 
 return {}
