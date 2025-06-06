b5a8ceaed779d73aa2d4e0c0082e21183244b0fa
Vamsi Talupula <tanavamsikrishna@outlook.com>
2025-06-07 01:09:39 +0530

    ''

--- lua/terminal_file_link_handler.lua
+++ lua/terminal_file_link_handler.lua
@@ -6,30 +6,43 @@ local function str_to_int(value)
   return math.floor(tonumber(value) or 0)
 end
 
+--- @param pwd string
 --- @param full_file_path string
 --- @param line string
 --- @return integer row, integer col
-local function parse_line(full_file_path, line)
-  -- /Users/vamsi/repo/stocks/system_trading/options_intraday/utils/read_tick_data_from_log.py:21:54 - error: "group" is not a known attribute of "None" (reportOptionalMemberAccess)
-  local row, col = line:match('.*' .. full_file_path .. ':(%d+):(%d+) - .*')
+local function parse_line(pwd, full_file_path, line)
+  local relative_file_path = vim.fs.relpath(pwd, full_file_path)
+
+  -- /Users/vamsi/repo/stocks/system_trading/options_intraday/utils/read_tick_data_from_log.py:21:54 - error: "group" ...
+  local row, col = line:match('%s*' .. full_file_path .. ':(%d+):(%d+) - .*')
   if row and col then
     return str_to_int(row), str_to_int(col)
   end
 
   -- File "/Users/vamsi/repo/playground/scratch.py", line 2, in func
-  local row = line:match('.*File "' .. full_file_path .. '", line (%d+),.*')
+  local row = line:match('%s*File "' .. full_file_path .. '", line (%d+),.*')
   if row then
     return str_to_int(row), 0
   end
+
+  -- test/test.py:11:8: F401 [*] `json` imported but unused
+  local row, col = line:match(relative_file_path .. ':(%d+):(%d+): ' .. '.*')
+  if row then
+    return str_to_int(row), str_to_int(col)
+  end
+
   return 0, 0
 end
 
-function M.open_file(pwd, file, whole_line)
+--- @param pwd string
+--- @param full_file_path string
+--- @param whole_line string
+function M.open_file(pwd, full_file_path, whole_line)
   if not vim.g.neovide then
     vim.notify 'Trying to open a file using `terminal_link_handler`. But this is not neovide'
   end
-  vim.api.nvim_cmd({ cmd = 'e', args = { file } }, {})
-  local row, col = parse_line(file, whole_line)
+  vim.api.nvim_cmd({ cmd = 'e', args = { full_file_path } }, {})
+  local row, col = parse_line(pwd, full_file_path, whole_line)
   vim.fn.cursor(row, col)
   vim.api.nvim_cmd({ cmd = 'NeovideFocus' }, {})
 end
