de9bbe6c73bc62a26d8b2161a90483d7ef7ee1d1
Vamsi Talupula <tanavamsikrishna@outlook.com>
2025-06-07 15:58:32 +0530

    ''

--- lua/terminal_file_link_handler.lua
+++ lua/terminal_file_link_handler.lua
@@ -13,12 +13,6 @@ end
 local function parse_line(pwd, full_file_path, line)
   local relative_file_path = vim.fs.relpath(pwd, full_file_path)
 
-  -- /Users/vamsi/repo/stocks/system_trading/options_intraday/utils/read_tick_data_from_log.py:21:54 - error: "group" ...
-  local row, col = line:match('%s*' .. full_file_path .. ':(%d+):(%d+) - .*')
-  if row and col then
-    return str_to_int(row), str_to_int(col)
-  end
-
   -- File "/Users/vamsi/repo/playground/scratch.py", line 2, in func
   local row = line:match('%s*File "' .. full_file_path .. '", line (%d+),.*')
   if row then
@@ -26,7 +20,9 @@ local function parse_line(pwd, full_file_path, line)
   end
 
   -- test/test.py:11:8: F401 [*] `json` imported but unused
-  local row, col = line:match(relative_file_path .. ':(%d+):(%d+): ' .. '.*')
+  --   --> src/main.rs:51:31
+  -- /Users/vamsi/repo/stocks/system_trading/options_intraday/utils/read_tick_data_from_log.py:21:54 - error: "group" ...
+  local row, col = line:match(relative_file_path .. ':(%d+):(%d+)')
   if row then
     return str_to_int(row), str_to_int(col)
   end
@@ -38,6 +34,7 @@ end
 --- @param full_file_path string
 --- @param whole_line string
 function M.open_file(pwd, full_file_path, whole_line)
+  -- print(vim.inspect { pwd, full_file_path, whole_line })
   if not vim.g.neovide then
     vim.notify 'Trying to open a file using `terminal_link_handler`. But this is not neovide'
   end
