1e0e1910ada8aa36108277cf15ec139a628395a2
Vamsi Talupula <tanavamsikrishna@outlook.com>
2025-06-12 11:13:17 +0530

    ''

--- lua/custom/config/miscellaneous.lua
+++ lua/custom/config/miscellaneous.lua
@@ -7,4 +7,7 @@ vim.api.nvim_set_hl(0, 'MiniStatuslineFilename', { fg = 'black', bg = 'white', i
 vim.opt.title = true
 vim.opt.titlestring = vim.fs.basename(vim.fn.getcwd())
 
+-- exrc
+vim.o.exrc = true
+
 return {}
--- lua/custom/plugins/conform.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'stevearc/conform.nvim',
-  opts = {
-    format_on_save = function()
-      -- Disable "format_on_save"
-      return nil
-    end,
-  },
-}
