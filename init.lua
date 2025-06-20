6a1fca5233525b1243f00df9e9d819d6a4f39ed8
Vamsi Talupula <tanavamsikrishna@outlook.com>
2025-06-20 18:30:25 +0530

    ''

--- init.lua
+++ init.lua
@@ -625,6 +625,7 @@ require('lazy').setup({
         pyright = {},
         rust_analyzer = {},
         nushell = {},
+        zls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
