94a3aa8fbf5690a869aaf51c04fd5e77172fb50d
Vamsi Talupula <tanavamsikrishna@outlook.com>
2025-07-04 15:00:48 +0530

    feat: Add yank ring, persistent neoclip, and buffer cleanup cmd

--- lua/custom/plugins/neoclip.lua
+++ lua/custom/plugins/neoclip.lua
@@ -2,8 +2,15 @@ return {
   'AckslD/nvim-neoclip.lua',
   dependencies = {
     { 'nvim-telescope/telescope.nvim' },
+    { 'kkharji/sqlite.lua' },
   },
   config = function()
-    require('neoclip').setup()
+    require('neoclip').setup {
+      history = 100,
+      enable_persistent_history = true,
+      db_path = '.neoclip.sqlite3',
+    }
+
+    vim.keymap.set('n', '"', require('telescope').extensions.neoclip.default, { desc = 'Registers' })
   end,
 }
--- lua/delete_ext_file_bufs.lua
+++ lua/delete_ext_file_bufs.lua
@@ -1,19 +1,20 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
-
-local function delete_external_file_bufs()
+-- Delete bufs of files which are not in the current working directory
+return function()
   local all_bufs = vim.api.nvim_list_bufs()
-  print(vim.inspect(all_bufs))
   local cwd = vim.fn.getcwd()
+  local is_some_buf_deleted = false
   for _, buf_id in ipairs(all_bufs) do
     local file_path = vim.api.nvim_buf_get_name(buf_id)
     if file_path ~= '' and file_path:find(cwd) == nil then
       print('Deleting buffer ' .. file_path)
       vim.api.nvim_buf_delete(buf_id, { force = false, unload = false })
+      is_some_buf_deleted = is_some_buf_deleted or true
     end
+  end
+  if not is_some_buf_deleted then
+    print 'No buffers deleted'
   end
 end
 
-return {}
+-- How to call?
+-- :lua require('delete_ext_file_bufs')()
