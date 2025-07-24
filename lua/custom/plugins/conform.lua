0b7a115df1e1a8a15172d0753e3c5a8d17dd427d
Vamsi Talupula <tanavamsikrishna@outlook.com>
2025-07-24 19:24:34 +0530

    feat(conform): add dockerfile formatter

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -9,12 +9,16 @@ return {
         command = 'json5',
         args = { '--space', '2' },
       },
+      dockerfmt = {
+        command = 'dockerfmt',
+      },
     },
     formatters_by_ft = {
       nu = { 'topiary_nushell' },
       python = { 'ruff_organize_imports', 'ruff_format' },
       json = { 'jq' },
       json5 = { 'json5' },
+      dockerfile = { 'dockerfmt' },
     },
   },
 }
