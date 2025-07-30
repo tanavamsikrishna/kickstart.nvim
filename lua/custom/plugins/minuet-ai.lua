9575d141cc1669fcccaf9ec45bb49c665cd75b8f
Vamsi Talupula <tanavamsikrishna@outlook.com>
2025-07-30 23:36:54 +0530

    feat(minuet-ai): add gemini provider support

--- lua/custom/plugins/minuet-ai.lua
+++ lua/custom/plugins/minuet-ai.lua
@@ -5,24 +5,31 @@ return {
   },
   config = function()
     require('minuet').setup {
-      provider = 'openai_fim_compatible',
-      n_completions = 1, -- recommend for local model for resource saving
-      -- I recommend beginning with a small context window size and incrementally
-      -- expanding it, depending on your local computing power. A context window
-      -- of 512, serves as an good starting point to estimate your computing
-      -- power. Once you have a reliable estimate of your local computing power,
-      -- you should adjust the context window to a larger value.
+      provider = 'gemini',
+      n_completions = 1,
       context_window = 2048,
       provider_options = {
-        openai_fim_compatible = {
-          -- For Windows users, TERM may not be present in environment variables.
-          -- Consider using APPDATA instead.
+        gemini = {
+          model = 'gemini-2.0-flash',
+          stream = true,
+          api_key = 'GEMINI_API_KEY',
+          end_point = 'https://generativelanguage.googleapis.com/v1beta/models',
+          optional = {
+            generationConfig = {
+              maxOutputTokens = 256,
+              thinkingConfig = {
+                thinkingBudget = 0,
+              },
+            },
+          },
+        },
+        ollama_qwen = {
           api_key = 'TERM',
           name = 'Ollama',
           end_point = 'http://localhost:11434/v1/completions',
           model = 'qwen2.5-coder:7b',
           optional = {
-            max_tokens = 20,
+            max_tokens = 56,
             top_p = 0.9,
           },
         },
