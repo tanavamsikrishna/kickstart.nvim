482343b346973dd7e11ef4a06b8b552024bb1959
Vamsi Talupula <tanavamsikrishna@outlook.com>
2025-07-30 18:39:21 +0530

    feat(minuet-ai): increase context window

--- lua/custom/plugins/minuet-ai.lua
+++ lua/custom/plugins/minuet-ai.lua
@@ -12,7 +12,7 @@ return {
       -- of 512, serves as an good starting point to estimate your computing
       -- power. Once you have a reliable estimate of your local computing power,
       -- you should adjust the context window to a larger value.
-      context_window = 512,
+      context_window = 2048,
       provider_options = {
         openai_fim_compatible = {
           -- For Windows users, TERM may not be present in environment variables.
