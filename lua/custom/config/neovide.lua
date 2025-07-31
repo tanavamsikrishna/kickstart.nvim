8a2a7ae14eec09b65865b9b544adc056a2696153
Vamsi Talupula <tanavamsikrishna@outlook.com>
2025-08-01 02:14:36 +0530

    style(neovide): update font and comment out unused options

--- lua/custom/config/neovide.lua
+++ lua/custom/config/neovide.lua
@@ -1,11 +1,11 @@
 if vim.g.neovide then
-  vim.o.guifont = 'Source Code Pro:h12'
+  vim.o.guifont = 'SauceCodePro Nerd Font:h12'
   -- vim.g.neovide_scroll_animation_length = 0
   vim.g.neovide_cursor_animation_length = 0
-  vim.opt.linespace = 1
+  -- vim.opt.linespace = 1
   vim.g.have_nerd_font = true
-  vim.g.neovide_text_gamma = 1.6
-  vim.g.neovide_text_contrast = 0.5
+  -- vim.g.neovide_text_gamma = 1.6
+  -- vim.g.neovide_text_contrast = 0.5
 
   -- Floating window config
   -- vim.g.neovide_floating_blur_amount_x = 0
