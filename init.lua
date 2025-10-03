603ec7e88f57cee987d201cdecd6cc562f45c255
Vamsi Talupula <tanavamsikrishna@outlook.com>
2025-10-03 20:02:50 +0530

    feat(lsp): add svelte lsp support

--- init.lua
+++ init.lua
@@ -627,6 +627,7 @@ require('lazy').setup({
         nushell = {},
         zls = {},
         taplo = {},
+        svelte = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
