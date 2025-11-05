ec785e8f10f92ef179faaf4950855afe1d082d6f
Vamsi Talupula <tanavamsikrishna@outlook.com>
2025-11-05 22:30:47 +0530

    chore(plugins): configure neo-tree and update plugin locks

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -1,8 +1,18 @@
 return {
   'nvim-neo-tree/neo-tree.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  lazy = false,
   opts = {
     sources = { 'filesystem', 'buffers', 'git_status', 'document_symbols' },
     window = { position = 'float' },
     default_source = 'last',
   },
+  config = function(_, opts)
+    require('neo-tree').setup(opts)
+    vim.api.nvim_set_hl(0, 'NeoTreeTitleBar', { link = 'NeoTreeFileStatsHeader' })
+  end,
 }
