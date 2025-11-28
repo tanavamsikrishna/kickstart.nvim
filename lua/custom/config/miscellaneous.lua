c402a75d7e2f1c78c47b4f1538256c67c3230ab4
Vamsi Talupula <tanavamsikrishna@outlook.com>
2025-11-28 15:55:27 +0530

    fix(ui): highlight matching parentheses

--- lua/custom/config/miscellaneous.lua
+++ lua/custom/config/miscellaneous.lua
@@ -2,6 +2,7 @@
 vim.api.nvim_set_hl(0, 'LspSignatureActiveParameter', { bold = true })
 vim.api.nvim_set_hl(0, 'DiagnosticFloatingHint', { link = 'DiagnosticFloatingInfo' })
 vim.api.nvim_set_hl(0, 'MiniStatuslineFilename', { fg = 'darkblue' })
+vim.api.nvim_set_hl(0, 'MatchParen', { bg = 'lightblue' })
 
 -- Set title to the current working directory
 vim.opt.title = true
