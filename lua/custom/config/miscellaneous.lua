565ec99d15294d4947003f02f901570d686ec3cd
Vamsi Talupula <tanavamsikrishna@outlook.com>
2026-01-06 15:52:51 +0530

    `style(ui): link telescopematching highlight to normal`

--- lua/custom/config/miscellaneous.lua
+++ lua/custom/config/miscellaneous.lua
@@ -3,6 +3,7 @@ vim.api.nvim_set_hl(0, 'LspSignatureActiveParameter', { bold = true })
 vim.api.nvim_set_hl(0, 'DiagnosticFloatingHint', { link = 'DiagnosticFloatingInfo' })
 vim.api.nvim_set_hl(0, 'MiniStatuslineFilename', { foreground = 'DarkSlateGray', background = 'white' })
 -- vim.api.nvim_set_hl(0, 'MatchParen', { bg = 'lightblue' })
+vim.api.nvim_set_hl(0, 'TelescopeMatching', { link = 'Normal' })
 
 -- Set title to the current working directory
 vim.opt.title = true
