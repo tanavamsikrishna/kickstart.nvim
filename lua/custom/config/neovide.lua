a4c5bd03679cee073d85316e41ca25db08abfaad
Vamsi Talupula <tanavamsikrishna@outlook.com>
2026-01-12 05:46:27 +0530

    chore(config): disable minuet-ai and configure neovide contrast

--- lua/custom/config/neovide.lua
+++ lua/custom/config/neovide.lua
@@ -4,8 +4,8 @@ if vim.g.neovide then
   vim.g.neovide_cursor_animation_length = 0
   -- vim.opt.linespace = 1
   vim.g.have_nerd_font = true
-  -- vim.g.neovide_text_gamma = 1.6
-  -- vim.g.neovide_text_contrast = 0.5
+  vim.g.neovide_text_gamma = 1.6
+  vim.g.neovide_text_contrast = 1
 
   -- Floating window config
   -- vim.g.neovide_floating_blur_amount_x = 0
