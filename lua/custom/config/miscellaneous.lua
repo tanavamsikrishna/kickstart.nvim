f135bb603eb5c8439e3ccfbfa7a5652fbbbb7e77
Vamsi Talupula <tanavamsikrishna@outlook.com>
2026-01-21 18:35:56 +0530

    `style(ui): set ministatusline filename color to darkgreen`

--- lua/custom/config/miscellaneous.lua
+++ lua/custom/config/miscellaneous.lua
@@ -1,7 +1,7 @@
 -- Fix UI issues
 vim.api.nvim_set_hl(0, 'LspSignatureActiveParameter', { bold = true })
 vim.api.nvim_set_hl(0, 'DiagnosticFloatingHint', { link = 'DiagnosticFloatingInfo' })
-vim.api.nvim_set_hl(0, 'MiniStatuslineFilename', { foreground = 'DarkSlateGray' })
+vim.api.nvim_set_hl(0, 'MiniStatuslineFilename', { foreground = 'darkgreen' })
 vim.api.nvim_set_hl(0, 'MatchParen', { bold = true, fg = 'black', underline = true })
 vim.api.nvim_set_hl(0, 'TelescopeMatching', { link = 'Normal' })
 
