b771291e3cd95241608168ae8ac27fb6631ec3e2
Vamsi Talupula <tanavamsikrishna@outlook.com>
2026-01-24 18:44:15 +0530

    chore(lint): disable markdownlint

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -6,7 +6,7 @@ return {
     config = function()
       local lint = require 'lint'
       lint.linters_by_ft = {
-        markdown = { 'markdownlint' },
+        -- markdown = { 'markdownlint' },
         lua = { 'luacheck' },
         dockerfile = { 'hadolint' },
         json5 = { 'json5' },
