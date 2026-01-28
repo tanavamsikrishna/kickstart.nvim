aed41da03dbd60d87863105f45441e59a39698f3
Vamsi Talupula <tanavamsikrishna@outlook.com>
2026-01-28 19:23:22 +0530

    refactor(plugins): move conform.nvim configuration to dedicated file

--- init.lua
+++ init.lua
@@ -611,51 +611,9 @@ require('lazy').setup({
       local capabilities = require('blink.cmp').get_lsp_capabilities()
       -- Sets capabilities for EVERY server globally
       vim.lsp.config('*', {
-        capabilities = require('blink.cmp').get_lsp_capabilities(),
+        capabilities = capabilities,
       })
     end,
-  },
-
-  { -- Autoformat
-    'stevearc/conform.nvim',
-    event = { 'BufWritePre' },
-    cmd = { 'ConformInfo' },
-    keys = {
-      {
-        '<localleader>f',
-        function()
-          require('conform').format { async = true, lsp_format = 'fallback' }
-        end,
-        mode = '',
-        desc = '[F]ormat buffer',
-      },
-    },
-    opts = {
-      notify_on_error = false,
-      format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
-        if disable_filetypes[vim.bo[bufnr].filetype] then
-          return nil
-        else
-          return {
-            timeout_ms = 500,
-            lsp_format = 'fallback',
-          }
-        end
-      end,
-      formatters_by_ft = {
-        lua = { 'stylua' },
-        fish = { 'fish_indent' },
-        toml = { 'taplo' },
-        python = { 'ruff_format' },
-        javascript = { 'prettierd', 'prettier' },
-        yaml = { 'yamlfmt' },
-      },
-      stop_after_first = true,
-    },
   },
 
   { -- Autocompletion
--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -1,6 +1,23 @@
 return {
   'stevearc/conform.nvim',
+  event = { 'BufWritePre' },
+  cmd = { 'ConformInfo' },
   opts = {
+    notify_on_error = true,
+    format_on_save = function(bufnr)
+      -- Disable "format_on_save lsp_fallback" for languages that don't
+      -- have a well standardized coding style. You can add additional
+      -- languages here or re-enable it for the disabled ones.
+      local disable_filetypes = { c = true, cpp = true }
+      if disable_filetypes[vim.bo[bufnr].filetype] then
+        return nil
+      else
+        return {
+          timeout_ms = 500,
+          lsp_format = 'fallback',
+        }
+      end
+    end,
     formatters = {
       topiary_nushell = {
         command = os.getenv 'SCRIPTS_FOLDER' .. '/bin/format.nu',
@@ -14,14 +31,19 @@ return {
       },
     },
     formatters_by_ft = {
+      css = { 'prettierd' },
+      dockerfile = { 'dockerfmt' },
+      fish = { 'fish_indent' },
+      html = { 'prettierd' },
+      javascript = { 'prettierd', 'prettier' },
+      json = { 'prettierd' },
+      json5 = { 'json5' },
+      lua = { 'stylua' },
       nu = { 'topiary_nushell' },
       python = { 'ruff_organize_imports', 'ruff_format' },
-      json = { 'jq' },
-      json5 = { 'json5' },
-      dockerfile = { 'dockerfmt' },
+      toml = { 'taplo' },
       yaml = { 'yamlfmt' },
-      css = { 'prettierd' },
-      html = { 'prettierd' },
     },
+    stop_after_first = true,
   },
 }
