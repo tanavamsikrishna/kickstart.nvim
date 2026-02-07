7abb298a47a1745a87288f1104e5c01a8749d8f7
Vamsi Talupula <tanavamsikrishna@outlook.com>
2026-02-07 16:56:56 +0530

    style(neovide): update font size to 12.5

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -1,6 +1,7 @@
 if vim.g.neovide then
   -- vim.o.guifont = 'JetBrainsMono Nerd Font:h13'
-  vim.o.guifont = 'Liga SFMono Nerd Font:h12'
+  vim.opt.guifont = { 'Liga SFMono Nerd Font:h12.5' }
+  -- vim.o.guifont = 'SauceCodePro Nerd Font'
   -- vim.g.neovide_scroll_animation_length = 0
   vim.g.neovide_position_animation_length = 0
   vim.g.neovide_cursor_animation_length = 0
