b6674bcf7c7e7373099b2006efc5a64cb86744ab
Vamsi Talupula <tanavamsikrishna@outlook.com>
2026-03-21 13:20:58 +0530

    chore(nvim): disable line numbers in init.lua

--- init.lua
+++ init.lua
@@ -13,7 +13,7 @@ vim.g.have_nerd_font = true
 --  For more options, you can see `:help option-list`
 
 -- Make line numbers default
-vim.o.number = true
+vim.o.number = false
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
 vim.o.relativenumber = true
